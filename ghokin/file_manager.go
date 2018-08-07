b4ba358d861d1cecc8b7689b7da198d5a87da424
Anthony HAMON <anthony.hamon@iadvize.com>
2018-08-07 21:39:39 +0200

    ghokin : add FileManager

---
+++ ghokin/file_manager.go
@@ -0,0 +1,43 @@
+package ghokin
+
+import (
+	"bytes"
+)
+
+type commands map[string]string
+
+type indent struct {
+	backgroundAndScenario int
+	step                  int
+	tableAndDocString     int
+}
+
+// FileManager handles transformation on feature files
+type FileManager struct {
+	indentConf indent
+	commands   commands
+}
+
+// NewFileManager creates a brand new FileManager, it requires indentation values and commands defined
+// as a shell commands in comments
+func NewFileManager(backgroundAndScenarioIndent int, stepIndent int, tableAndDocStringIndent int, commands map[string]string) FileManager {
+	return FileManager{
+		indent{
+			backgroundAndScenarioIndent,
+			stepIndent,
+			tableAndDocStringIndent,
+		},
+		commands,
+	}
+}
+
+// Transform formats and apply shell commands on feature file
+func (f FileManager) Transform(filename string) (bytes.Buffer, error) {
+	section, err := extractSections(filename)
+
+	if err != nil {
+		return bytes.Buffer{}, err
+	}
+
+	return transform(section, f.indentConf, f.commands)
+}
