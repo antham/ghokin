c94b616d6640e64a07e524ae68ce3edcaa0c1fbe
Anthony HAMON <anthony.hamon@iadvize.com>
2018-08-11 23:49:23 +0200

    ghokin : add TransformAndReplace to file manager

--- ghokin/file_manager.go
+++ ghokin/file_manager.go
@@ -2,6 +2,11 @@ package ghokin
 
 import (
 	"bytes"
+	"fmt"
+	"os"
+	mpath "path"
+	"path/filepath"
+	"sync"
 )
 
 type commands map[string]string
@@ -40,4 +45,116 @@ func (f FileManager) Transform(filename string) (bytes.Buffer, error) {
 	}
 
 	return transform(section, f.indentConf, f.commands)
+}
+
+// TransformAndReplace formats and applies shell commands on file or folders
+// and replace the content of files
+func (f FileManager) TransformAndReplace(path string, extensions []string) []error {
+	errors := []error{}
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		return append(errors, err)
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		errors = append(errors, f.replaceFolderWithContent(path, extensions)...)
+	case mode.IsRegular():
+		b, err := f.Transform(path)
+
+		if err != nil {
+			return append(errors, err)
+		}
+
+		if err := replaceFileWithContent(path, b.Bytes()); err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	return errors
+}
+
+func (f FileManager) replaceFolderWithContent(path string, extensions []string) []error {
+	errors := []error{}
+	fc := make(chan string)
+	wg := sync.WaitGroup{}
+	var mu sync.Mutex
+	defer mu.Unlock()
+
+	files, err := findFeatureFiles(path, extensions)
+
+	if err != nil {
+		return []error{err}
+	}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func() {
+			for file := range fc {
+				b, err := f.Transform(file)
+
+				if err != nil {
+					mu.Lock()
+					errors = append(errors, fmt.Errorf(`An error occurred with file "%s" : %s`, file, err.Error()))
+					mu.Unlock()
+					continue
+				}
+
+				if err := replaceFileWithContent(file, b.Bytes()); err != nil {
+					mu.Lock()
+					errors = append(errors, fmt.Errorf(`An error occurred with file "%s" : %s`, file, err.Error()))
+					mu.Unlock()
+				}
+			}
+
+			wg.Done()
+		}()
+	}
+
+	for _, file := range files {
+		fc <- file
+	}
+
+	close(fc)
+	wg.Wait()
+	mu.Lock()
+
+	return errors
+}
+
+func replaceFileWithContent(filename string, content []byte) error {
+	file, err := os.Create(filename)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(content)
+
+	return err
+}
+
+func findFeatureFiles(rootPath string, extensions []string) ([]string, error) {
+	files := []string{}
+
+	if err := filepath.Walk(rootPath, func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		for _, extension := range extensions {
+			if !info.IsDir() && mpath.Ext(p) == "."+extension {
+				files = append(files, p)
+				break
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return []string{}, err
+	}
+
+	return files, nil
 }
--- ghokin/file_manager_test.go
+++ ghokin/file_manager_test.go
@@ -2,7 +2,9 @@ package ghokin
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -40,5 +42,241 @@ func TestFileManagerTransform(t *testing.T) {
 		)
 
 		scenario.test(f.Transform(scenario.filename))
+	}
+}
+
+func TestFileManagerTransformAndReplace(t *testing.T) {
+	type scenario struct {
+		path       string
+		extensions []string
+		setup      func()
+		test       func([]error)
+	}
+
+	scenarios := []scenario{
+		{
+			"/tmp/ghokin/file1.feature",
+			[]string{"feature"},
+			func() {
+				content := []byte(`Feature: test
+   test
+
+Scenario:            scenario1
+   Given       whatever
+   Then                  whatever
+"""
+hello world
+"""
+`)
+
+				assert.NoError(t, os.RemoveAll("/tmp/ghokin"))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin", 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/file1.feature", content, 0777))
+			},
+			func(errs []error) {
+				assert.Len(t, errs, 0)
+
+				content := `Feature: test
+      test
+
+  Scenario: scenario1
+    Given whatever
+    Then whatever
+      """
+      hello world
+      """`
+
+				b, e := ioutil.ReadFile("/tmp/ghokin/file1.feature")
+
+				assert.NoError(t, e)
+				assert.EqualValues(t, content, string(b))
+			},
+		},
+		{
+			"/tmp/ghokin/",
+			[]string{"feature"},
+			func() {
+				content := []byte(`Feature: test
+   test
+
+Scenario:   scenario%d
+   Given           whatever
+   Then      whatever
+"""
+hello world
+"""`)
+
+				assert.NoError(t, os.RemoveAll("/tmp/ghokin"))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin", 0777))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin/test1", 0777))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin/test2/test3", 0777))
+
+				for i, f := range []string{
+					"/tmp/ghokin/file1.feature",
+					"/tmp/ghokin/file2.feature",
+					"/tmp/ghokin/test1/file3.feature",
+					"/tmp/ghokin/test1/file4.feature",
+					"/tmp/ghokin/test2/test3/file5.feature",
+					"/tmp/ghokin/test2/test3/file6.feature",
+				} {
+					assert.NoError(t, ioutil.WriteFile(f, []byte(fmt.Sprintf(string(content), i)), 0777))
+				}
+			},
+			func(errs []error) {
+				assert.Len(t, errs, 0)
+
+				content := `Feature: test
+      test
+
+  Scenario: scenario%d
+    Given whatever
+    Then whatever
+      """
+      hello world
+      """`
+
+				for i, f := range []string{
+					"/tmp/ghokin/file1.feature",
+					"/tmp/ghokin/file2.feature",
+					"/tmp/ghokin/test1/file3.feature",
+					"/tmp/ghokin/test1/file4.feature",
+					"/tmp/ghokin/test2/test3/file5.feature",
+					"/tmp/ghokin/test2/test3/file6.feature",
+				} {
+					b, e := ioutil.ReadFile(f)
+
+					assert.NoError(t, e)
+					assert.EqualValues(t, fmt.Sprintf(content, i), string(b))
+				}
+			},
+		},
+		{
+			"/tmp/ghokin/",
+			[]string{"feature"},
+			func() {
+				content := []byte(`Feature: test
+   test
+
+Scenario:   scenario
+   Given           whatever
+   Then      whatever
+"""
+hello world
+"""`)
+
+				assert.NoError(t, os.RemoveAll("/tmp/ghokin"))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin", 0777))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin/test1", 0777))
+
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/file1.feature", content, 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/file2.feature", append([]byte("whatever"), content...), 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/test1/file3.feature", content, 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/test1/file4.feature", content, 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/test1/file5.feature", append([]byte("whatever"), content...), 0777))
+			},
+			func(errs []error) {
+				assert.Len(t, errs, 2)
+
+				msgs := []string{
+					`An error occurred with file "/tmp/ghokin/file2.feature" : Parser errors:
+(1:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'whateverFeature: test'`,
+					`An error occurred with file "/tmp/ghokin/test1/file5.feature" : Parser errors:
+(1:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'whateverFeature: test'`,
+				}
+
+				for _, e := range errs {
+					var match bool
+					for _, msg := range msgs {
+						if msg == e.Error() {
+							match = true
+						}
+					}
+
+					if !match {
+						assert.Fail(t, "Must fail with 2 files when formatting folder")
+					}
+				}
+			},
+		},
+		{
+			"/tmp/ghokin/",
+			[]string{"txt", "feat"},
+			func() {
+				content := []byte(`Feature: test
+   test
+
+Scenario:   scenario
+   Given           whatever
+   Then      whatever
+"""
+hello world
+"""`)
+
+				assert.NoError(t, os.RemoveAll("/tmp/ghokin"))
+				assert.NoError(t, os.MkdirAll("/tmp/ghokin", 0777))
+
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/file1.feature", content, 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/file2.txt", content, 0777))
+				assert.NoError(t, ioutil.WriteFile("/tmp/ghokin/file3.feat", content, 0777))
+			},
+			func(errs []error) {
+				assert.Len(t, errs, 0)
+
+				contentFormatted := `Feature: test
+      test
+
+  Scenario: scenario
+    Given whatever
+    Then whatever
+      """
+      hello world
+      """`
+
+				contentUnformatted := `Feature: test
+   test
+
+Scenario:   scenario
+   Given           whatever
+   Then      whatever
+"""
+hello world
+"""`
+
+				for _, s := range []struct {
+					filename string
+					expected string
+				}{
+					{
+						"/tmp/ghokin/file1.feature",
+						contentUnformatted,
+					},
+					{
+						"/tmp/ghokin/file2.txt",
+						contentFormatted,
+					},
+					{
+						"/tmp/ghokin/file3.feat",
+						contentFormatted,
+					},
+				} {
+					b, e := ioutil.ReadFile(s.filename)
+
+					assert.NoError(t, e)
+					assert.EqualValues(t, s.expected, string(b))
+				}
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.setup()
+
+		f := NewFileManager(2, 4, 6,
+			map[string]string{
+				"seq": "seq 1 3",
+			},
+		)
+
+		scenario.test(f.TransformAndReplace(scenario.path, scenario.extensions))
 	}
 }
