43e2ffaede6bc3ad48d59f4791dc6950c96a79a0
Anthony HAMON <anthony.hamon@iadvize.com>
2018-08-14 12:39:32 +0200

    cmd : add tests

---
+++ cmd/fixtures/feature.feature
@@ -0,0 +1,18 @@
+Feature: Test
+      This is a description
+
+  Background:
+    Given something
+
+  Scenario: A scenario to test
+    Given a thing
+    Given something else
+    Then something happened
+
+  Scenario: Another scenario to test
+    Given a second thing
+    Given another second thing
+    Then another thing happened
+      """
+      test
+      """
---
+++ cmd/fmt_stdout_test.go
@@ -0,0 +1,95 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"sync"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatOnStdout(t *testing.T) {
+	var code int
+	var w sync.WaitGroup
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	viper.Set("indent.backgroundAndScenario", 2)
+	viper.Set("indent.step", 4)
+	viper.Set("indent.tableAndDocString", 6)
+
+	msgHandler := messageHandler{
+		func(exitCode int) {
+			panic(exitCode)
+		},
+		&stdout,
+		&stderr,
+	}
+
+	w.Add(1)
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				code = r.(int)
+			}
+
+			w.Done()
+		}()
+
+		cmd := &cobra.Command{}
+		args := []string{"fixtures/feature.feature"}
+
+		formatOnStdout(msgHandler, cmd, args)
+	}()
+
+	w.Wait()
+
+	b, err := ioutil.ReadFile("fixtures/feature.feature")
+
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, 0, code, "Must exit with no errors (exit 0)")
+	assert.EqualValues(t, string(b), stdout.String())
+}
+
+func TestFormatOnStdoutWithErrors(t *testing.T) {
+	var code int
+	var w sync.WaitGroup
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	msgHandler := messageHandler{
+		func(exitCode int) {
+			panic(exitCode)
+		},
+		&stdout,
+		&stderr,
+	}
+
+	w.Add(1)
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				code = r.(int)
+			}
+
+			w.Done()
+		}()
+
+		cmd := &cobra.Command{}
+		args := []string{"fixtures/featurefeature.feature"}
+
+		formatOnStdout(msgHandler, cmd, args)
+	}()
+
+	w.Wait()
+
+	assert.EqualValues(t, 1, code, "Must exit with errors (exit 1)")
+	assert.EqualValues(t, "open fixtures/featurefeature.feature: no such file or directory\n", stderr.String())
+}
