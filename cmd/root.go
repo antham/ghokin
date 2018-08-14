55db03b70a0f933cc7600e31b1b392dc6f7e6437
Anthony HAMON <anthony.hamon@iadvize.com>
2018-08-14 23:15:23 +0200

    cmd : various fixes
    * set message handler as an argument for testing purpose
    * log json error if commands get a bad-formed JSON string
    * log parsing error if config file can't be parsed

--- cmd/root.go
+++ cmd/root.go
@@ -24,49 +24,56 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig(newMessageHandler()))
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file")
 }
 
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := homedir.Dir()
+func initConfig(msgHandler messageHandler) func() {
+	return func() {
+		if cfgFile != "" {
+			viper.SetConfigFile(cfgFile)
+		} else {
+			home, err := homedir.Dir()
 
-		if err != nil {
-			newMessageHandler().errorFatal(err)
+			if err != nil {
+				msgHandler.errorFatal(err)
+			}
+
+			viper.AddConfigPath(home)
+			viper.AddConfigPath(".")
+			viper.SetConfigName(".ghokin")
 		}
 
-		viper.AddConfigPath(home)
-		viper.AddConfigPath(".")
-		viper.SetConfigName(".ghokin-fmt")
-	}
-
-	viper.SetEnvPrefix("ghokin")
-	for _, err := range []error{
-		viper.BindEnv("indent.backgroundAndScenario"),
-		viper.BindEnv("indent.step"),
-		viper.BindEnv("indent.tableAndDocString"),
-	} {
-		if err != nil {
-			newMessageHandler().errorFatal(err)
+		viper.SetEnvPrefix("ghokin")
+		for _, err := range []error{
+			viper.BindEnv("indent.backgroundAndScenario"),
+			viper.BindEnv("indent.step"),
+			viper.BindEnv("indent.tableAndDocString"),
+		} {
+			if err != nil {
+				msgHandler.errorFatal(err)
+			}
 		}
-	}
 
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+		viper.AutomaticEnv()
 
-	viper.SetDefault("indent.backgroundAndScenario", 2)
-	viper.SetDefault("indent.step", 4)
-	viper.SetDefault("indent.tableAndDocString", 6)
+		viper.SetDefault("indent.backgroundAndScenario", 2)
+		viper.SetDefault("indent.step", 4)
+		viper.SetDefault("indent.tableAndDocString", 6)
 
-	commands := map[string]string{}
-	if err := json.Unmarshal([]byte(viper.GetString("commands")), &commands); err == nil {
+		commands := map[string]string{}
+		if err := json.Unmarshal([]byte(viper.GetString("commands")), &commands); viper.IsSet("commands") && err != nil {
+			msgHandler.errorFatalStr("check commands is a well-formed JSON : " + err.Error())
+		}
+
 		viper.SetDefault("commands", commands)
-	}
 
-	if err := viper.ReadInConfig(); err != nil {
-		return
+		if err := viper.ReadInConfig(); err != nil {
+			switch err.(type) {
+			case viper.ConfigParseError:
+				msgHandler.errorFatalStr("check your yaml config file is well-formed : " + err.Error())
+			}
+		}
 	}
 }
